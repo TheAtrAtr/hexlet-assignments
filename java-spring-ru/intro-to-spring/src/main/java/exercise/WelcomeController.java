94747b1412678a0e45af39cf792a332080e87f4e
@hexlet/cli <support@hexlet.io>
2023-03-13 14:57:00 +0300

    submit java-spring-ru/intro-to-spring

---
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+@RestController
+class WelcomeController {
+    @GetMapping()
+    public String wellcomme(){
+        return "Welcome to Spring";
+    }
+
+    @GetMapping("/hello")
+    public String wellcomme(@RequestParam(value = "name", defaultValue = "World") String string){
+        return String.format("Hello %s!", string);
+    }
+        }
+// END

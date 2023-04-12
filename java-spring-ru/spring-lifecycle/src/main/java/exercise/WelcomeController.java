45d46e6f900730b341922fbef72583d0f9e639bf
@hexlet/cli <support@hexlet.io>
2023-04-12 11:28:28 +0300

    submit java-spring-ru/spring-lifecycle

---
+++ java-spring-ru/spring-lifecycle/src/main/java/exercise/WelcomeController.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import exercise.daytimes.Daytime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+@RestController
+public class WelcomeController {
+    @Autowired
+    Daytime daytime;
+    @Autowired
+    Meal meal;
+
+    @GetMapping(path = "daytime")
+    public String getMessage() {
+        return String.format("It is %s now. Enjoy your %s", daytime.getName(), meal.getMealForDaytime(daytime.getName()));
+    }
+}
+// END
